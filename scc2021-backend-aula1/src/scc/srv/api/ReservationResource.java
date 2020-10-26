13f0e7c25f2f734c3e132048d1072d6c1c5e12b1
Henrique Realinho <46897048+hrealinho@users.noreply.github.com>
2020-10-26 00:22:21 +0000

    reservation resource init

--- scc2021-backend-aula1/src/scc/srv/api/ReservationResource.java
+++ scc2021-backend-aula1/src/scc/srv/api/ReservationResource.java
@@ -1,5 +1,33 @@
 package scc.srv.api;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import scc.data.Reservation;
+
+@Path("/reservations")
 public interface ReservationResource {
 
+	@POST
+	@Path("")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Reservation addReservation(Reservation reservation);
+	
+	@GET
+	@Path("{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Reservation getReservation(@PathParam("id") String id);
+	
+	@DELETE
+	@Path("{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Reservation deleteReservation(@PathParam("id") String id);
+		
 }
