0cf506c3c601bed35c2aa4b7eb141604ce388658
Antonio Beirao <AntonioBeirao@users.noreply.github.com>
2020-11-16 06:04:43 +0000

    update calendar/calendarService

--- scc2021-backend/src/scc/srv/api/services/CalendarService.java
+++ scc2021-backend/src/scc/srv/api/services/CalendarService.java
@@ -89,25 +89,36 @@ public class CalendarService {
 		return calendar;
 	}
 
-	public Map<String, String> getCalendarEntry(String id, String date) {
 
-		
-		return null;
-	}
-
-
-	public Iterator<Pair<Date, Date>> getAvailablePeriods(String calendarId){
+	public Date[] getAvailablePeriods(String calendarId){
 		//ideia, e o stor fala "information about available periods", ou seja uma lista com periodos de datas ? que ele tem disponivel, pode ser uma valor default, tipo ves 30 dias do dia
 		//	da querie, ou com queries personalizadas mas isso ]e to much pa mim
-		Calendar calendar = this.get(calendarId);
-
-
 		//Isto só é possivel dps de perceber como é suposto date ser uma string e nao haver uma lista organizada por datas no calendar
 
+		
+		/*
+		 * Dei esta try -> mas agora damos update a lista de availableDays qdo criamos uma reservation
+		 */
+		Calendar calendar = this.get(calendarId);
+		
+		try {
+			calendar = mapper.readValue(jedis.get(CALENDAR_KEY_PREFIX + calendarId), Calendar.class);
+		
+			if (calendar == null) {
+				calendar = cosmosDB.getCalendar(calendarId);
+				jedis.set(CALENDAR_KEY_PREFIX + calendarId, mapper.writeValueAsString(calendar));
+			}
+			
+			Date[] availableDays = calendar.getAvailableDays();
+			return availableDays;
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
 
 
-
-		return null;
 	}
 
 	public Iterator<Reservation> getReservations(String calendarId){
