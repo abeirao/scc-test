c99eba94487abe48deadf2f3a05885e5dc622318
Antonio Beirao <AntonioBeirao@users.noreply.github.com>
2020-12-14 01:46:07 +0000

    Started Postgress impl.

--- scc2021-backend/src/scc/data/Database.java
+++ scc2021-backend/src/scc/data/Database.java
@@ -1,5 +1,41 @@
 package scc.data;
 
-public class Database {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Optional;
+import java.util.logging.Logger;
+import java.util.logging.Level;
 
+
+public class Database {    
+
+	
+	String entities = "CREATE TABLE IF NOT EXISTS entities (id serial PRIMARY KEY, name VARCHAR(25))";
+	
+    int id = 6;
+    String entity = "Trygve Gulbranssen";
+    String query = "INSERT INTO entities(id, name) VALUES(?, ?)";
+    
+    
+    public void putEntity(String entity) {
+
+		try (Connection con = JDBCConnection.getConnection();
+	         PreparedStatement pst = con.prepareStatement(query)) {
+	        
+	        pst.setInt(1, id);
+	        pst.setString(2, entity);
+	        pst.executeUpdate();
+	
+	    } catch (SQLException ex) {
+	
+	        Logger lgr = Logger.getLogger(Database.class.getName());
+	        lgr.log(Level.SEVERE, ex.getMessage(), ex);
+	    }
+    }
 }
+	
+	
+
+
