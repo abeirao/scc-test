613fcfbdbf230fcf07cb84e5621911e6a68ecda2
hrealinho <h.realinho@campus.fct.unl.pt>
2020-12-14 02:31:34 +0000

    db

--- scc2021-backend/src/scc/data/Database.java
+++ scc2021-backend/src/scc/data/Database.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Iterator;
 import java.util.Optional;
 import java.util.logging.Logger;
 
@@ -41,12 +42,65 @@ public class Database {
 	}
 	
 	public Entity getEntityById( String id) {
-		Optional<Entity> entity = entities.get(id);
-		
+		Optional<Entity> entity = entities.get(Long.parseLong(id));
+		return entity.isEmpty() ? null: entity.get();
 	}
 
 	public Iterator<Entity> getEntities() {
 		return entities.getAll().iterator();
+	}
+// forums
+	public void delForum(Forum forum) {
+		forums.delete(forum);
+	}
+
+	public void putForum(Forum forum) {
+		forums.save(forum);
+	}
+
+	public Forum getForumById(String id) {
+		Optional<Forum> forum = forums.get(Long.parseLong(id));
+		return forum.isEmpty() ? null : forum.get();
+	}
+
+	public Iterator<Forum> getForums() {
+		return forums.getAll().iterator();
+	}
+
+// reservations
+	public void delReservation(Reservation reservation) {
+		reservations.delete(reservation);
+	}
+
+	public void putReservation(Reservation reservation) {
+		reservations.save(reservation);
+	}
+
+	public Reservation getReservationById(String id) {
+		Optional<Reservation> reservation = reservations.get(Long.parseLong(id));
+		return reservation.isEmpty() ? null : reservation.get();
+	}
+
+	public Iterator<Reservation> getReservations() {
+		return reservations.getAll().iterator();
+	}
+
+// calendars
+	public void delCalendar(Calendar calendar) {
+		calendars.delete(calendar);
+	}
+
+	public void putCalendar(Calendar calendar) {
+		calendars.save(calendar);
+	}
+
+	public Calendar getCalendarById(String id) {
+		Optional<Calendar> calendar = calendars.get(Long.parseLong(id));
+		return calendar.isEmpty() ? null : calendar.get();
+	}
+
+	public Iterator<Calendar> getCalendars() {
+		return calendars.getAll().iterator();
 	}
 }
 	
