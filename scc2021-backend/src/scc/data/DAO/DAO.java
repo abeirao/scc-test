1ec2c730125080737d9ea8cba852be72c679084f
hrealinho <h.realinho@campus.fct.unl.pt>
2020-12-14 09:41:27 +0000

    reservation dao save method

--- scc2021-backend/src/scc/data/DAO/DAO.java
+++ scc2021-backend/src/scc/data/DAO/DAO.java
@@ -5,9 +5,14 @@ import java.util.Optional;
 
 // Data Access Object
 public interface DAO<T, I> {
+	
     Optional<T> get(Long id);
+    
     Collection<T> getAll();
+    
     Optional<I> save(T t);
+    
     void update(T t);
+    
     void delete(T t);
 }
--- scc2021-backend/src/scc/data/DAO/ReservationDAO.java
+++ scc2021-backend/src/scc/data/DAO/ReservationDAO.java
@@ -6,6 +6,8 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.Collection;
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Optional;
 
 import scc.data.JDBCConnection;
@@ -33,9 +35,63 @@ public class ReservationDAO implements DAO<Reservation, Long> {
             stmt.executeUpdate(query);
         } catch (Exception e) {
             e.printStackTrace();
-        }
-        
+        }        
     }
+
+
+    public Optional<Reservation> get(Long id) {
+        Reservation reservation = findReservation(id);
+        if (reservation == null)
+            return Optional.empty();
+        return Optional.of(reservation);
+    }
+
+    public Collection<Reservation> getAll() {
+        List<Reservation> reservations = getReservations();
+        return reservations;
+    }
+
+	public Optional<Long> save(Reservation reservation) {
+        try {
+			Connection conn = JDBCConnection.getConnection(); 
+			
+			String sql = "INSERT INTO " + RESERVATIONS +
+                    	" (id, name, entityId, day) VALUES (?, ?, ?, ?)";
+			
+			// the use of PreparedStatement prevents SQL injection 
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			stmt.setString(1, reservation.getId().toString());
+			stmt.setString(2, reservation.getName());
+			stmt.setString(3, reservation.getEntityId());
+			stmt.setDate(4, (java.sql.Date) reservation.getDay());			
+			
+			int rows = stmt.executeUpdate();
+			return Optional.of(Long.parseLong(reservation.getId()));
+        } catch (Exception e) {
+        	
+        }
+    }
+
+    public void update(Reservation reservation) {
+    	
+    }
+
+    public void delete(Reservation reservation) {
+        try {
+			Connection conn = JDBCConnection.getConnection(); 
+			
+			String sql = "DELETE * FROM " + RESERVATIONS + " WHERE id=?;";
+			 
+			// the use of PreparedStatement prevents SQL injection 
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			stmt.setString(1, reservation.getId().toString());
+			
+			int rows = stmt.executeUpdate();
+        } catch (Exception e) {
+        	
+        }
+    }
+    
 
     private Reservation findReservation(Long id) {
         try {
@@ -71,26 +127,38 @@ public class ReservationDAO implements DAO<Reservation, Long> {
             return null;
         }
     }
-
-    public Optional<Reservation> get(Long id) {
-        Reservation reservation = findReservation(id);
-        if (reservation == null)
-            return Optional.empty();
-        return Optional.of(reservation);
-    }
-
-    public Collection<Reservation> getAll() {
-        return null;
-    }
-
-    public Optional<Long> save(Reservation reservation) {
-        return null;
-    }
-
-    public void update(Reservation reservation) {
-    }
-
-    public void delete(Reservation reservation) {
-    }
+    
+    private List<Reservation> getReservations() {
+    	List<Reservation> reservations = new LinkedList<Reservation>();
+        try {
+			Connection conn = JDBCConnection.getConnection(); 
+			
+			String sql = "SELECT * FROM " + RESERVATIONS + "";
+			// the use of PreparedStatement prevents SQL injection 
+			PreparedStatement stmt = conn.prepareStatement(sql);			
+			ResultSet rs = stmt.executeQuery(); // ResultSet is a Cursor
+			
+			while (rs.next()) {	// if the user exists
+				String rid = rs.getString("id"); // get by column label
+				String name = rs.getString("name"); 
+                String entityId = rs.getString("entityId"); 
+                Date day = rs.getDate("day");
+											
+                Reservation reservation = new Reservation();
+                reservation.setId(rid.toString());
+                reservation.setName(name);
+                reservation.setEntityId(entityId);
+                reservation.setDay(day);
+                
+				reservations.add(reservation);
+            }
+			rs.close();
+			stmt.close();
+			conn.close();
+	    	return reservations;
+        } catch (Exception e) {
+            return null;
+        }
+	}
 
 }
